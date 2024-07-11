84ca6494d2f2ff15f1a0ece848d48cc44001bd66
nostalfinals <yuu8583@proton.me>
2024-07-12 05:02:52 +0800

    build: module

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,28 @@ plugins {
     alias(libs.plugins.kotlin)
 }
 
-group = "hashi"
+group = "dev.hashimc.hashi"
 version = "0.1.0-SNAPSHOT"
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    }
+
+    kotlin {
+        javaToolchains {
+            version = "17"
+        }
+    }
+
     repositories {
         mavenCentral()
         mavenLocal()
         google()
-        maven(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
-        maven(uri("https://repo.papermc.io/repository/maven-public/"))
-        maven(uri("https://maven.nostal.ink/repository/maven-public"))
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://maven.nostal.ink/repository/maven-public")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,5 @@
 rootProject.name = "hashi"
 
+include("core", "core:core-common")
+include("loader")
+include("bootstrap")
